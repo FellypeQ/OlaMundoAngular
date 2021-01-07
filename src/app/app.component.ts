e89389507688b4caf709cf73fea2e0180a9f8b7a
FellypeQ <fellype.ads@gmail.com>
2021-01-07 10:10:25 -0300

    alterado html para ola mundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ola-mundo';
+  title = 'O teste inicial';
+  farinha = ['banana', 'acabate', 'limão'];
 }
